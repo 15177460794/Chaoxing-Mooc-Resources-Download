a1929c6360ea470f2ff594dadd9790fb222a5c4f
Reviona <32984956+Reviona@users.noreply.github.com>
2021-12-03 18:03:14 +0800

    Update 学习通资源下载.user.js

--- 学习通资源下载.user.js
+++ 学习通资源下载.user.js
@@ -1,23 +1,45 @@
 // ==UserScript==
-// @name         学习通资源下载
+// @name         超星课程资料下载
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  下载学习通“章节”模块中的视频、PPT等资源。
-// @author       Reviona
-// @match        *://*/mycourse/studentstudy?*
-// @icon         https://www.google.com/s2/favicons?domain=chaoxing.com
+// @version      0.3
+// @description  给超星中不能下载的文件添加下载按钮
+// @author       jungtravor
+// @match        *.chaoxing.com/coursedata*
+// @match        *.chaoxing.com/mycourse/studentstudy*
+// @icon         http://erya.mooc.chaoxing.com/assets/images/common/favicon.ico
 // @grant        none
 // ==/UserScript==
 
-(function()
-    {
-    var download = utils_1.CssBtn(utils_2.createBtn("下载资源",, "cx-btn"));
-    download.style.background = "#999999";
-    download.onclick = function () {
-        utils_2.get("https://cs-ans.chaoxing.com/download/" + _this.task.taskinfo.property.objectid, function (data) {
-            var json = JSON.parse(data);
-            prompt("如果打开下载失败，请复制下面链接手动下载", json.download);
-            window.open(json.download);
-        }).error(function () {
-            alert("资源信息获取失败");
+(function() {
+    'use strict';
+    const $ = window.jQuery;
+    const dURL = 'https://cs-ans.chaoxing.com/download/';
+    // 资料下载
+    var zycCount = 0;
+    $("div.ZYCon").find("tr").each(function(){
+        console.log($(this)[0]);
+        var objectID = $(this).attr("objectid");
+        if(objectID && ($(this).find("a.fr.download").length===0)){
+            $(this).find("div.fr.zybtn").prepend('<a href="'+dURL+objectID+'" class="fr download"></a>');
+            zycCount++
+        }
+    });
+    if(zycCount) console.log("总共更改"+zycCount+"处");
+    // 课件下载
+    var iframeCount = 0;
+    var interval = setInterval(function(){
+        if(!$("#qqqq")) clearInterval(interval);
+        var a = $("#iframe").contents().find(".ans-attach-ct iframe").length;
+        if(a==0) return;
+        var count = 0;
+        $("#iframe").contents().find(".ans-attach-ct iframe").each(function(){
+            var data = JSON.parse($(this).attr('data'));
+            var objectid = data.objectid;
+            if(objectid&&$(this).parent().find("#hk4d").length==0){
+                var bt = '<a id="hk4d" href="'+dURL+objectid+'"><button style="float:right;cursor:pointer;background-color: #7C98FF;color:#FFFFFF;border:none;height:2rem;width:5rem;margin-bottom:1rem;">下载</button></a>';
+                $(this).before(bt);
+                iframeCount++;
+            }
+        })
+    }, 1000);
 })();
